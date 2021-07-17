557c354092585580521dcfcfed36670d1de9e70b
Cristian Arroyo L <cristian.arroyo@utp.ac.pa>
2021-07-16 20:16:03 -0500

    Nueva linea en MiClase.java

--- src/main/java/MiClase.java
+++ src/main/java/MiClase.java
@@ -2,6 +2,10 @@ public class MiClase{
 
     public static void main(String[] args) {
         System.out.println("Este es un proyecto Java con Maven");
+
+        System.out.println("Nueva linea");
+
+
     }
 
 }
